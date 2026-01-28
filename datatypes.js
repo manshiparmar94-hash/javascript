f9a5790c57dc303b5d167b960be6ed7b635fc50e
Your Name <you@example.com>
2026-01-28 02:55:55 -0500

    [MB][variable & datatypes files  are added]

---
+++ datatypes.js
@@ -0,0 +1,143 @@
+// two types of data types
+//1.primitve
+// Number
+//BigInt
+//Boolean
+//Null
+//undefined
+
+
+//2.non primitve 
+//Array
+//Object
+//Function
+
+
+//let num=1;
+//let num2=2;
+
+//console.log("num",num,null2);
+
+//let name="manshi"
+//let name2="mahek"
+
+//console.timeLog("name :",name2,name)
+//Boolean
+//let isTrue=true;
+//let isfalse=false;
+
+// console.leg("boolean:")
+
+//underfiend
+//let a;
+//console.leg("ubdefind:",a)
+//letBigInt.log(BigInt)
+
+
+//2.non primitve
+//boject
+
+//let student={
+    //name:"manshi",
+    //roll:56,
+    //address:{
+       // city:maliya
+    //}
+  //  }
+//}
+
+
+//console.log(student);
+
+
+//Array
+
+//let a=[1,2,3,4,5,6,7,8,9];
+//let student={
+//}
+// let arrayofstud =[{},{}]
+//console.log(a);
+
+//console.log(a);
+
+
+//function
+
+function greet(){
+    console.log("this is function");
+}
+greet();
+
+
+function showvalues(value){
+    console.log("this is value:",value);
+    if(value==19){
+        console.log("number");
+    }
+}
+showvalues();
+showvalues(19);
+showvalues([1,2,3,4,5]);
+showvalues();
+
+
+function addition(a,b){
+    console.log(a+b);
+}
+
+addition(10,20);
+
+function multiplication(a,b){
+    console.log(a*b);
+}
+multiplication(10,20);
+
+function division(a,b){
+    console.log(a/b);
+}
+division(10,20);
+
+function substraction(a,b){
+    console.log(a-b);
+}
+substraction(10,20);
+
+
+const add =(num1 = 5,num2) = 5=> {
+    return num1 + num2
+    console.log()
+}
+
+//console.log('this is arrow fun :',sub())
+
+const sub=(num1,num2)=>{
+    return num1-num2
+    console.log()
+}
+ 
+
+//console.log('this is arrow fun :',division())
+
+const division=(num1,num2)=>{
+    return num1/num2
+    console.log()
+}
+
+//console.log('this is arrow fun :',multiplication())
+
+const multiplication=(num1,num2)=>{
+    return num1*num2
+    console.log()
+}
+
+let value =10;
+console.log('DEFUALT VALUE :',value);
+
+vslue -= 5 // value + 5
+console.log(value)
+
+vslue *= 5 // value * 5
+console.log(value)
+
+vslue /= 5 // value / 5
+console.log(value)
---
+++ variable.js
@@ -0,0 +1,11 @@
+//var a=10;
+//var a=20;
+//console.log(a);
+let a=20;
+if(true){
+    let a=10;
+}
+console.log(a);
+//const
+const pi=3.14;
+console.log(pi);
