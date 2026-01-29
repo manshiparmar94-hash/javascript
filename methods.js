704b60621ac7b7fc3ff8a077fdd2e4923d954fb5
Your Name <you@example.com>
2026-01-28 23:27:35 -0500

    [MB][methods & templetliterals are added]

---
+++ methods.js
@@ -0,0 +1,21 @@
+// string method
+// length()
+// trim()
+//tolowrcase()
+
+//includes()
+//indexof()
+//slice()
+//substring()
+//replace()
+//cocat()
+//split()
+
+let str="hello javascript world"
+console.lof(str)
+console.log(str.length)
+
+let trimText = str.trim();
+ console.log("train() :",trimText.length)
+
+ console.log("toUppercase() :",str.toUpperCase())
---
+++ templetliterals.js
@@ -0,0 +1,5 @@
+let  fname ="manshi";
+let lname="parmar";
+
+let message = 'heloo students,i am $(fname) $(lname)'
+console.log(message);
